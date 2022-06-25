2f6fec9c6fe37c919838d988df7e5035f2068b41
rusikstayhome <alenrus32@gmail.com>
2022-06-25 21:32:59 +0300

    added a unit conversion

--- src/app.js
+++ src/app.js
@@ -24,7 +24,9 @@ function displayTemperature(response) {
     let dateElement = document.querySelector('#date');
     let iconElement = document.querySelector('#icon');
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    celsiusTemperature = response.data.main.temp;
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
@@ -46,9 +48,36 @@ function handleSubmit(event) {
     search(cityInputElement.value);
 }
 
-search('Warsaw')
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector('#temperature');
 
+    celsiusLink.classList.remove('active');
+    fahrenheitLink.classList.add('active');
 
+    let fahrenheitTemperature = (temperatureElement.innerHTML * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector('#temperature');
+
+    fahrenheitLink.classList.remove('active');
+    celsiusLink.classList.add('active');
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector('#search-form');
 form.addEventListener('submit', handleSubmit);
+
+let fahrenheitLink = document.querySelector('#fahrenheit-link');
+fahrenheitLink.addEventListener('click', displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector('#celsius-link');
+celsiusLink.addEventListener('click', displayCelsiusTemperature);
+
+search('Warsaw');
