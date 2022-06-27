70c7aae150b2a0375fbef49bdb6fc82bd654aa61
rusikstayhome <alenrus32@gmail.com>
2022-06-27 15:33:56 +0300

    made a forecast from next day

--- src/app.js
+++ src/app.js
@@ -18,9 +18,9 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
-    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let days = ['', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-    return days[day];
+    return days[day + 1];
 }
 
 function displayForecast(response) {
